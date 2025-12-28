b90775904ffdb54e92a71aeca93c6efc1c030703
PlanetDestroyyer <wrieddude@gmail.com>
2025-12-28 13:08:42 +0530

    Update losses __init__.py with Tier 2 exports

--- state_core/losses/__init__.py
+++ state_core/losses/__init__.py
@@ -3,5 +3,14 @@ Loss functions for SOSM training.
 """
 
 from .block_regularization import OrthogonalityLoss, VarianceLoss
+from .contrastive_loss import ContextContrastiveLoss, BlockContrastiveLoss
+from .auxiliary_tasks import AuxiliaryTaskLoss, BlockUsageBalancing
 
-__all__ = ['OrthogonalityLoss', 'VarianceLoss']
+__all__ = [
+    'OrthogonalityLoss', 
+    'VarianceLoss', 
+    'ContextContrastiveLoss', 
+    'BlockContrastiveLoss',
+    'AuxiliaryTaskLoss',
+    'BlockUsageBalancing'
+]
